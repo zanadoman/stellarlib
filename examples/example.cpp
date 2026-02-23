167814b1491fa4413b0551008c37e555a29102bc
zanadoman <domanseven@gmail.com>
2026-02-23 17:04:41 +0000

    world example

--- examples/example.cpp
+++ examples/example.cpp
@@ -21,14 +21,43 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
+#include <stellarlib/ecs/world.hpp>
 #include <stellarlib/stellarlib.hpp>
 
 #include <cstdint>
+#include <cstdio>
 #include <cstdlib>
+#include <iostream>
+#include <string>
 
 auto main([[maybe_unused]] const std::int32_t argc, [[maybe_unused]] const char **argv)
 	-> std::int32_t
 {
 	stellarlib::hello_world();
+	stellarlib::ecs::world world{};
+	const auto player1{world.spawn(std::string{"Player 0"}, std::uint16_t{0})};
+	std::puts("--------------------------------------------------------------------------------");
+
+	for (const auto [entity, name, score] : world.query<std::string, std::uint16_t>()) {
+		std::cout << entity << ": { name: " << *name << ", score: " << *score << " }\n";
+	}
+
+	const auto player2{world.spawn(std::uint16_t{5}, std::string{"Player 1"})};
+	std::puts("--------------------------------------------------------------------------------");
+
+	for (const auto [entity, score, name] : world.query<std::uint16_t, std::string>()) {
+		std::cout << entity << ": { name: " << *name << ", score: " << *score << " }\n";
+	}
+
+	const auto player3{world.spawn(std::string{"Player 2"}, std::uint16_t{10})};
+	std::puts("--------------------------------------------------------------------------------");
+
+	for (const auto [entity, name, score] : world.query<std::string, std::uint16_t>()) {
+		std::cout << entity << ": { name: " << *name << ", score: " << *score << " }\n";
+	}
+
+	world.despawn(player3);
+	world.despawn(player2);
+	world.despawn(player1);
 	return EXIT_SUCCESS;
 }
--- include/stellarlib/ecs/world.hpp
+++ include/stellarlib/ecs/world.hpp
@@ -29,10 +29,14 @@
 #include <stellarlib/ecs/sparse_set.hpp>
 #include <stellarlib/ecs/sparse_storage.hpp>
 #include <stellarlib/ecs/stack_vector.hpp>
+#include <stellarlib/ext/utility.hpp>
 
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
 #include <utility>
+#include <tuple>
+#include <ranges>
 
 namespace stellarlib::ecs
 {
@@ -56,12 +60,117 @@ public:
 
 	constexpr ~world() noexcept = default;
 
+	template <typename ...T>
+	constexpr auto spawn(T &&...components) noexcept
+	{
+		auto entity{_entities.size()};
+
+		if (_queue.size()) {
+			entity = *(_queue.end() - 1);
+			_queue.pop();
+		}
+
+		_bitset.clear();
+
+		([&]() -> void {
+			const auto id{_components.id<T>()};
+			_bitset.insert(id);
+			_components.at<T>(id).insert(entity, std::forward<T>(components));
+		}(), ...);
+
+		const auto archetype{std::ranges::find_if(_archetypes, [this](const auto &pair) -> bool {
+			return pair.first == _bitset;
+		})};
+
+		if (archetype == _archetypes.end()) {
+			_entities.insert(entity, _archetypes.size());
+			_archetypes.push(std::pair{_bitset, internal::sparse_set<std::uint32_t>{}});
+			(_archetypes.end() - 1)->second.insert(entity);
+
+			for (auto &index : _indices) {
+				if (index.first <= (_archetypes.end() - 1)->first) {
+					index.second.push(_archetypes.size() - 1);
+				}
+			}
+		}
+		else {
+			archetype->second.insert(entity);
+			_entities.insert(entity, static_cast<std::size_t>(archetype - _archetypes.begin()));
+		}
+
+		return entity;
+	}
+
+	[[nodiscard]]
+	constexpr auto query() const noexcept
+	{
+		return _entities.keys();
+	}
+
+	template <typename T>
+	[[nodiscard]]
+	constexpr auto query() noexcept
+	{
+		return _components.at<T>().zip();
+	}
+
+	template <typename ...T>
+	[[nodiscard]]
+	constexpr auto query() noexcept
+		requires (1 < sizeof...(T))
+	{
+		const auto id{ext::scoped_typeid<world, std::tuple<T...>>()};
+
+		if (_queries.extend(id + 1, static_cast<std::size_t>(-1)) || _queries[id] == static_cast<std::size_t>(-1)) {
+			_bitset.clear();
+			(_bitset.insert(_components.id<T>()), ...);
+
+			const auto index{std::ranges::find_if(_indices, [this](const auto &pair) -> bool {
+				return pair.first == _bitset;
+			})};
+
+			if (index == _indices.end()) {
+				_queries[id] = _indices.size();
+				_indices.push(std::pair{_bitset, internal::stack_vector<std::size_t>{}});
+
+				for (const auto i : std::views::iota(std::size_t{}, _archetypes.size())) {
+					if (_bitset <= _archetypes[i].first) {
+						(_indices.end() - 1)->second.push(i);
+					}
+				}
+			}
+			else {
+				_queries[id] = static_cast<std::size_t>(index - _indices.begin());
+			}
+		}
+
+		return _indices[_queries[id]].second
+			| std::views::transform([this](const auto index) -> internal::sparse_set<std::uint32_t> & {
+				return _archetypes[index].second;
+			})
+			| std::views::join
+			| std::views::transform([this](const auto entity) -> std::tuple<std::uint32_t, T *...> {
+				return {entity, std::addressof(_components.operator[]<T>()[entity])...};
+			});
+	}
+
+	constexpr auto despawn(const std::uint32_t entity) noexcept
+	{
+		if (const auto index{_entities.at(entity)}) {
+			_entities.erase(entity);
+			_archetypes[*index].second.erase(entity);
+			_components.erase(entity);
+			_queue.push(entity);
+		}
+	}
+
 private:
-	internal::sparse_storage _components;
-	internal::stack_vector<std::size_t, std::uint32_t> _entities;
-	internal::stack_vector<std::pair<internal::bitset, internal::sparse_set<std::uint32_t>>> _archetypes;
-	internal::sparse_map<std::size_t, std::pair<internal::bitset, internal::stack_vector<std::size_t>>> _queries;
 	internal::stack_vector<std::uint32_t> _queue;
+	internal::sparse_map<std::uint32_t, std::size_t> _entities;
+	internal::stack_vector<std::pair<internal::bitset, internal::sparse_set<std::uint32_t>>> _archetypes;
+	internal::sparse_storage _components;
+	internal::stack_vector<std::size_t, std::size_t> _queries;
+	internal::stack_vector<std::pair<internal::bitset, internal::stack_vector<std::size_t>>> _indices;
 	internal::bitset _bitset;
 };
 }
