4c460e57db6261c617634987dc747d783e183833
zanadoman <domanseven@gmail.com>
2026-06-09 16:13:00 +0200

    fix android orientation

--- examples/healthcheck.cpp
+++ examples/healthcheck.cpp
@@ -29,9 +29,10 @@
 #include <stellarlib/ext/filesystem.hpp>
 #include <stellarlib/stellarlib.hpp>
 
+#include <SDL3/SDL_assert.h>
 #include <SDL3/SDL_gpu.h>
+#include <SDL3/SDL_log.h>
 
-#include <cassert>
 #include <memory>
 #include <optional>
 #include <string>
@@ -46,59 +47,105 @@ namespace
 {
 constexpr void check_metadata(const app::context &ctx)
 {
-	assert(ctx.metadata().name() == "healthcheck");
-	assert(ctx.metadata().version() == "0.1.0");
-	assert(ctx.metadata().identifier() == "org.stellarlib.healthcheck");
-	assert(ctx.metadata().creator() == "Domán Zana");
-	assert(ctx.metadata().copyright() == "Copyright (C) 2025-2026 Domán Zana");
-	assert(ctx.metadata().url() == "https://zanadoman.github.io/stellarlib/examples_2healthcheck_8cpp-example.html");
-	assert(ctx.metadata().type() == "application");
+	SDL_assert_always(ctx.metadata().name() == "healthcheck");
+	SDL_assert_always(ctx.metadata().version() == "0.1.0");
+	SDL_assert_always(ctx.metadata().identifier() == "org.stellarlib.healthcheck");
+	SDL_assert_always(ctx.metadata().creator() == "Domán Zana");
+	SDL_assert_always(ctx.metadata().copyright() == "Copyright (C) 2025-2026 Domán Zana");
+	SDL_assert_always(ctx.metadata().url() == "https://zanadoman.github.io/stellarlib/examples_2healthcheck_8cpp-example.html");
+	SDL_assert_always(ctx.metadata().type() == "application");
 }
 
 constexpr void check_clock(app::context &ctx)
 {
-	assert(0.0F < ctx.clock().now());
-	assert(ctx.clock().target_frequency() == 60.0F);
+	SDL_assert_always(0.0F < ctx.clock().now());
+	SDL_assert_always(ctx.clock().target_frequency() == 60.0F);
 	ctx.clock().set_target_frequency(120.0F);
-	assert(ctx.clock().target_frequency() == 120.0F);
-	assert(ctx.clock().frame() < ctx.clock().now());
-	assert(ctx.clock().max_delta() == 0.05F);
+	SDL_assert_always(ctx.clock().target_frequency() == 120.0F);
+	SDL_assert_always(ctx.clock().frame() < ctx.clock().now());
+	SDL_assert_always(ctx.clock().max_delta() == 0.05F);
 	ctx.clock().set_max_delta(0.025F);
-	assert(ctx.clock().max_delta() == 0.025F);
-	assert(!lin::cast<bool>(ctx.clock().delta()));
+	SDL_assert_always(ctx.clock().max_delta() == 0.025F);
+	SDL_assert_always(!lin::cast<bool>(ctx.clock().delta()));
 }
 
 constexpr void check_window(app::context &ctx)
 {
-	assert(ctx.window().title() == "org.stellarlib.healthcheck");
+	SDL_assert_always(ctx.window().title() == "org.stellarlib.healthcheck");
 	ctx.window().set_title("healthcheck");
-	assert(ctx.window().title() == "healthcheck");
-	assert(!lin::all(ctx.window().resolution()));
-	assert(!ctx.window().focused());
-	assert(static_cast<const SDL_GPUDevice *>(ctx.window().renderer()));
-	assert(static_cast<std::shared_ptr<SDL_GPUDevice>>(ctx.window().renderer()).get() == static_cast<const SDL_GPUDevice *>(ctx.window().renderer()));
+	SDL_assert_always(ctx.window().title() == "healthcheck");
+	SDL_assert_always(!lin::all(ctx.window().resolution()));
+	SDL_assert_always(!ctx.window().focused());
+	SDL_assert_always(static_cast<const SDL_GPUDevice *>(ctx.window().renderer()));
+	SDL_assert_always(static_cast<std::shared_ptr<SDL_GPUDevice>>(ctx.window().renderer()).get() == static_cast<const SDL_GPUDevice *>(ctx.window().renderer()));
 	ctx.window().renderer().set_vsync(!ctx.window().renderer().vsync());
 	const res::image image{ext::filesystem::base_directory_path() / "assets" / "tests" / "linear.png"};
 	const auto texture1{ctx.window().renderer().upload_image(image, false)};
-	assert(static_cast<SDL_GPUTexture *>(texture1));
-	assert(static_cast<const SDL_GPUDevice *>(texture1) == static_cast<const SDL_GPUDevice *>(ctx.window().renderer()));
-	assert(lin::all(texture1.size() == image.size()));
-	assert(!texture1.mipmaps());
-	assert(ctx.window().renderer().download_texture(texture1, false) == image);
+	SDL_assert_always(static_cast<SDL_GPUTexture *>(texture1));
+	SDL_assert_always(static_cast<const SDL_GPUDevice *>(texture1) == static_cast<const SDL_GPUDevice *>(ctx.window().renderer()));
+	SDL_assert_always(lin::all(texture1.size() == image.size()));
+	SDL_assert_always(!texture1.mipmaps());
+	SDL_assert_always(ctx.window().renderer().download_texture(texture1, false) == image);
 	const auto texture2{ctx.window().renderer().upload_image(image, true)};
-	assert(static_cast<SDL_GPUTexture *>(texture2));
-	assert(static_cast<const SDL_GPUDevice *>(texture2) == static_cast<const SDL_GPUDevice *>(ctx.window().renderer()));
-	assert(lin::all(texture2.size() == image.size()));
-	assert(texture2.mipmaps());
-	assert(ctx.window().renderer().download_texture(texture2, true) == image);
+	SDL_assert_always(static_cast<SDL_GPUTexture *>(texture2));
+	SDL_assert_always(static_cast<const SDL_GPUDevice *>(texture2) == static_cast<const SDL_GPUDevice *>(ctx.window().renderer()));
+	SDL_assert_always(lin::all(texture2.size() == image.size()));
+	SDL_assert_always(texture2.mipmaps());
+	SDL_assert_always(ctx.window().renderer().download_texture(texture2, true) == image);
 }
+
+class [[nodiscard]] blank final : public app::scene
+{
+public:
+	[[nodiscard]]
+	constexpr blank()
+	{
+		SDL_Log("%p: constructor", static_cast<const void *>(this));
+	}
+
+	[[nodiscard]]
+	constexpr blank(const blank &) noexcept = delete;
+
+	[[nodiscard]]
+	constexpr blank(blank &&) noexcept = delete;
+
+	constexpr auto operator=(const blank &) noexcept
+		-> blank & = delete;
+
+	constexpr auto operator=(blank &&) noexcept
+		-> blank & = delete;
+
+	constexpr ~blank() final
+	{
+		SDL_Log("%p: destructor", static_cast<const void *>(this));
+	}
+
+private:
+	constexpr void begin([[maybe_unused]] app::context &ctx) final
+	{
+		SDL_Log("%p: begin", static_cast<const void *>(this));
+	}
+
+	[[nodiscard]]
+	constexpr auto update([[maybe_unused]] app::context &ctx)
+		-> std::optional<std::unique_ptr<app::scene>> final
+	{
+		SDL_Log("%p: update", static_cast<const void *>(this));
+		return std::nullopt;
+	}
+
+	constexpr void end([[maybe_unused]] app::context &ctx) final
+	{
+		SDL_Log("%p: end", static_cast<const void *>(this));
+	}
+};
 }
 
 auto app::init(const std::vector<std::string> &args)
 	-> std::optional<info>
 {
-	assert(!args.empty());
-	assert(!args.front().empty());
+	SDL_assert_always(!args.empty());
+	SDL_assert_always(!args.front().empty());
 
 	return {{
 		.metadata = {
@@ -126,7 +173,7 @@ auto app::init(const std::vector<std::string> &args)
 			check_metadata(ctx);
 			check_clock(ctx);
 			check_window(ctx);
-			return nullptr;
+			return std::make_unique<blank>();
 		}
 	}};
 }
